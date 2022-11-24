5e86f77e51a6be662a296a812d30dae2a2d75a9f
jicking bebiro <7297029+jicking@users.noreply.github.com>
2022-11-24 04:09:32 -0500

    Read and display env var

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+
+  let envMode = import.meta.env.MODE
+  let envVariableValue = import.meta.env.VITE_GREETINGS
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
-
+  <div>
+    Mode: {envMode} <br>
+    Variable value: {envVariableValue} <br>
+  </div>
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
